5de6aa9faff6e8060677cac4ae70dc08d6d9221f
Ben Goerdt <benjamin-goerdt@uiowa.edu>
2014-03-05 13:37:36 -0600

    Created initial project

---
+++ FSE_Subscription_App/FSE_Subscription_App/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FSE_Subscription_App.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Message = "YAY we have a project created.";
+
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your app description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
